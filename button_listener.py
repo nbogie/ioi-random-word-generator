79f13b17dff3a7e8d10f9ddf4992be4cb71283e2
nbogie <neillbogie@googlemail.com>
2019-07-20 16:55:23 +0100

    Create button_listener.py

---
+++ button_listener.py
@@ -0,0 +1,30 @@
+import RPi.GPIO as GPIO
+import os
+from time import sleep
+
+def button_callback(channel):
+	keyName = "Tab"
+	if channel == 23:
+		keyName = "ctrl+Tab"
+	elif channel == 24:
+		keyName = "2"
+	elif channel == 25:
+		keyName = "3"
+	print("button falling edge detected pin:" + str(channel) + " so sending keypress " + keyName)
+	os.system('export XAUTHORITY=/home/pi/.Xauthority; export DISPLAY=:0; xdotool key '+keyName)
+        
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(23, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(24, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(25, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+GPIO.add_event_detect(23,GPIO.FALLING,callback=button_callback, bouncetime=200)
+GPIO.add_event_detect(24,GPIO.FALLING,callback=button_callback, bouncetime=200)
+GPIO.add_event_detect(25,GPIO.FALLING,callback=button_callback, bouncetime=200)
+
+print("Will send keypress when pin 23 24 or 25 sees falling edge")
+
+while True:
+    sleep(5)
+
+GPIO.cleanup()
