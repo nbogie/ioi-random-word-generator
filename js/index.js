53bc997e2d29e32a1f61197cd6edc5e45f29b52a
Neill Bogie <neillbogie@googlemail.com>
2019-07-20 17:31:57 +0100

    listen for onkeydown.  long words only (testing)

--- js/index.js
+++ js/index.js
@@ -1,16 +1,33 @@
 "use strict";
 //assumes that wordList.js will already have been loaded
 
+const allWords = WORDLIST;
+const longWords = WORDLIST.sort((a, b) => b.length - a.length).slice(0, 10);
+
 function pick(arr) {
   const ix = Math.floor(Math.random() * arr.length);
   return arr[ix];
 }
 
-function showNewWord() {
+function showRandomWord() {
+  showInGiantWord(pick(allWords));
+}
+function showLongWord() {
+  showInGiantWord(pick(longWords));
+}
+
+function showInGiantWord(str) {
   const elem = document.getElementById("giantword");
   const colorHex = _.shuffle(["ff", "00", "aa"]).join("");
   elem.style.color = "#" + colorHex;
-  elem.innerText = pick(WORDLIST);
+  elem.innerText = str;
 }
 
-showNewWord();
+showRandomWord();
+
+document.body.onkeydown = handleKeypress;
+
+function handleKeypress(e) {
+  console.log({ handlingKeypress: e.code });
+  showLongWord();
+}
